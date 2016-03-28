86a5a6fb97025e1edad9a5577789792220b6d1bb
Felipe <falcantara@webeleven.com.br>
2016-03-28 17:14:53 -0300

    Ajustes

--- app/Vinci/Infrastructure/Orders/DoctrineOrderRepository.php
+++ app/Vinci/Infrastructure/Orders/DoctrineOrderRepository.php
@@ -8,10 +8,12 @@ use Vinci\Infrastructure\Common\DoctrineBaseRepository;
 class DoctrineOrderRepository extends DoctrineBaseRepository implements OrderRepository
 {
 
-    public function getAllCustomerOrders($customerId)
+    public function getByCustomer($customerId, $perPage = 10, $pageName = 'page')
     {
         $query = $this->_em->createQuery("SELECT o, oi, c, p FROM \Vinci\Domain\Order\Order o JOIN o.items oi JOIN oi.product p JOIN o.customer c WHERE c.id = :customer_id");
         $query->setParameter('customer_id', $customerId);
-        return $query->getResult();
+
+        return $this->paginate($query, $perPage, $pageName);
     }
+
 }
