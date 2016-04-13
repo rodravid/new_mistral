b7b565f29bd982a5a386d8f980d7efa6114d462a
Felipe <falcantara@webeleven.com.br>
2016-04-13 14:00:10 -0300

    Ajustes

--- app/Vinci/App/Cms/resources/views/dashboard/index.blade.php
+++ app/Vinci/App/Cms/resources/views/dashboard/index.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 
-        <!-- Content Header (Page header) -->
+    <!-- Content Header (Page header) -->
     <section class="content-header">
         <h1>
             Dashboard
             <small>Control panel</small>
         </h1>
         <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li><a href="{{ route('cms.dashboard') }}"><i class="fa fa-dashboard"></i> Home</a></li>
             <li class="active">Dashboard</li>
         </ol>
     </section>
