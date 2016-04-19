5830db9a5dd4484525c2f5e55cf52a42331b0264
Felipe <falcantara@webeleven.com.br>
2016-04-19 10:21:28 -0300

    Ajustes

--- app/Vinci/App/Cms/resources/views/customers/edit.blade.php
+++ app/Vinci/App/Cms/resources/views/customers/edit.blade.php
@@ -4,7 +4,7 @@
     <ol class="breadcrumb">
         <li><a href="/cms"><i class="fa fa-dashboard"></i> Home</a></li>
         <li><a href="{{ $currentModule->getUrl() }}"><i class="{{ $currentModule->getIcon() }}"></i> {{ $currentModule->getTitle() }}</a></li>
-        <li class="active"><i class="fa fa-edit"></i> Editando usuário #{{ $user->getId() }}</li>
+        <li class="active"><i class="fa fa-edit"></i> {{ $currentModule->getEditingText() }} #{{ $highlight->getId() }}</li>
     </ol>
 @endsection
 
@@ -13,47 +13,26 @@
     <section class="content">
         <div class="row">
 
-            {!! Form::model($user, ['route' => ['cms.users.edit#update', $user->getId()], 'method' => 'PUT', 'files' => true]) !!}
+            {!! Form::model($customer, ['route' => ['cms.' . $currentModule->getName() . '.edit#update', $customer->getId()], 'method' => 'PUT', 'files' => true]) !!}
 
             <div class="col-xs-12 col-lg-9">
                 <div class="box box-primary">
                     <div class="box-header with-border">
-                        <h3 class="box-title">Editar usuário #{{ $user->getId() }}</h3>
+                        <h3 class="box-title">{{ $currentModule->getEditingText() }} #{{ $highlight->getId() }}</h3>
                     </div>
 
-                    {!! Form::hidden('id', $user->getId()) !!}
+                    {!! Form::hidden('id', $customer->getId()) !!}
 
                     <div class="box-body">
-
-                        @include('cms::users.form')
-
-                        <div class="form-group">
-                            <label for="selectUserRoles">Grupo</label>
-                            <select name="roles" id="selectUserRoles" class="form-control select2">
-                                <option value="">Selecione o grupo</option>
-                                @foreach($roles as $role)
-                                    <option value="{{ $role->getId() }}" @if(old('roles') == $role->getId() || $user->hasRole($role)) selected @endif>{{ $role->getTitle() }}</option>
-                                @endforeach
-                            </select>
-                        </div>
+                        @include('cms::customers.form')
                     </div>
 
                 </div>
             </div>
 
             <div class="col-xs-12 col-lg-3">
-                @include('cms::layouts.partials.publication.edit.default', ['model' => $user])
+                @include('cms::layouts.partials.publication.edit.default', ['model' => $customer])
             </div>
-
-            @if ($user->hasProfilePhoto())
-                <div class="col-xs-12 col-lg-3">
-                    @include('cms::layouts.partials.image.default', [
-                    'box_title' => 'Foto do perfil',
-                    'image' => $user->getProfilePhoto(),
-                    'delete_url' => route('cms.users.edit#remove-photo', [$user->getId(), $user->getProfilePhoto()->getId()])
-                    ])
-                </div>
-            @endif
 
             {!! Form::close() !!}
 
