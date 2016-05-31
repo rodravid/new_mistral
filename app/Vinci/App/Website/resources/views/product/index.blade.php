0bdad954e5c3db57774d6c615f43ac14edcd79d6
TCavallini <tcavallini@webeleven.com.br>
2016-05-31 19:41:39 -0300

    programacao da paginad e produto mobile

--- app/Vinci/App/Website/resources/views/product/index.blade.php
+++ app/Vinci/App/Website/resources/views/product/index.blade.php
@@ -74,7 +74,9 @@
                             <li>
                                 <ul>
                                     <li><p class="item-info-wine">País</p></li>
-                                    <li><a href="{{ $product->country->getWebUrl() }}"><p class="info-vinho-template">{{ $product->country->name }}</p></a></li>
+                                    <li><a href="{{ $product->country->getWebUrl() }}"><p
+                                                    class="info-vinho-template">{{ $product->country->name }}</p></a>
+                                    </li>
                                 </ul>
                             </li>
                         @endif
@@ -83,7 +85,9 @@
                             <li>
                                 <ul>
                                     <li><p class="item-info-wine">Região</p></li>
-                                    <li><a href="{{ $product->region->getWebUrl() }}"><p class="info-vinho-template">{{ $product->region->name }}</p></a></li>
+                                    <li><a href="{{ $product->region->getWebUrl() }}"><p
+                                                    class="info-vinho-template">{{ $product->region->name }}</p></a>
+                                    </li>
                                 </ul>
                             </li>
                         @endif
@@ -92,7 +96,9 @@
                             <li>
                                 <ul>
                                     <li><p class="item-info-wine">Tipo</p></li>
-                                    <li><a href="{{ $product->productType->getWebUrl() }}"><p class="info-vinho-template">{{ $product->productType->name }}</p></a>
+                                    <li><a href="{{ $product->productType->getWebUrl() }}"><p
+                                                    class="info-vinho-template">{{ $product->productType->name }}</p>
+                                        </a>
                                     </li>
                                 </ul>
                             </li>
@@ -146,7 +152,8 @@
                     </div>
 
                     <div class="content-img-product">
-                        <img src="{{ asset_web('images/no_photo.png') }}" class="img-product" alt="{{ $product->title }}">
+                        <img src="{{ asset_web('images/no_photo.png') }}" class="img-product"
+                             alt="{{ $product->title }}">
 
                         @if($scores = $product->getHighlightedScores())
 
@@ -178,23 +185,30 @@
                             <div class="wrap-add" ng-init="ctrl.itemQuantity = 1">
                                 <span>GARRAFA</span>
                                 <div class="botoes-add">
-                                    <a id="remover-unidade" class="bt-remove" href="javascript:void(0);" ng-click="ctrl.decrementItemQuantity()">-</a>
-                                    <input type="text" id="mudarUnidade" class="input-quantity" value="1" ng-model="ctrl.itemQuantity" ng-blur="ctrl.updateItemQuantity()">
-                                    <a id="add-unidade" href="javascript:void(0);" class="bt-add" ng-click="ctrl.incrementItemQuantity()">+</a>
+                                    <a id="remover-unidade" class="bt-remove" href="javascript:void(0);"
+                                       ng-click="ctrl.decrementItemQuantity()">-</a>
+                                    <input type="text" id="mudarUnidade" class="input-quantity" value="1"
+                                           ng-model="ctrl.itemQuantity" ng-blur="ctrl.updateItemQuantity()">
+                                    <a id="add-unidade" href="javascript:void(0);" class="bt-add"
+                                       ng-click="ctrl.incrementItemQuantity()">+</a>
                                 </div>
                             </div>
                             <div class="wrap-add" ng-init="ctrl.boxQuantity = 0; ctrl.boxQuantityFactor = 12">
                                 <span>CAIXA <br> (12 garrafas)</span>
                                 <div class="botoes-add">
-                                    <a id="remover-unidade" class="bt-remove" href="javascript:void(0);" ng-click="ctrl.decrementBoxQuantity()">-</a>
-                                    <input type="text" id="mudarUnidade" class="input-quantity" value="0" ng-model="ctrl.boxQuantity" ng-blur="ctrl.updateBoxQuantity()">
-                                    <a id="add-unidade" href="javascript:void(0);" class="bt-add" ng-click="ctrl.incrementBoxQuantity()">+</a>
+                                    <a id="remover-unidade" class="bt-remove" href="javascript:void(0);"
+                                       ng-click="ctrl.decrementBoxQuantity()">-</a>
+                                    <input type="text" id="mudarUnidade" class="input-quantity" value="0"
+                                           ng-model="ctrl.boxQuantity" ng-blur="ctrl.updateBoxQuantity()">
+                                    <a id="add-unidade" href="javascript:void(0);" class="bt-add"
+                                       ng-click="ctrl.incrementBoxQuantity()">+</a>
                                 </div>
                             </div>
                         </div>
 
                         <a class="bt-default-full-template bt-big template-button" cart-add-button
-                           variant-id="{{ $product->getMasterVariant()->getId() }}" quantity-resolver="ctrl.getQuantityForCart()" href="#">Comprar <span
+                           variant-id="{{ $product->getMasterVariant()->getId() }}"
+                           quantity-resolver="ctrl.getQuantityForCart()" href="#">Comprar <span
                                     class="arrow-link">></span></a>
 
                         <div class="content-term-delivery show-desktop">
@@ -206,137 +220,83 @@
 
                     <ul class="details-wine show-mobile">
 
-                        <li>
-                            <ul>
-                                <li><p class="item-info-wine">Produtor</p></li>
-                                <li><a href="/produtor/catena-zapata"><p class="info-vinho-template">Catena Zapata</p></a>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <ul>
-                                <li><p class="item-info-wine">País</p></li>
-                                <li><a href="/pais/argentina"><p class="info-vinho-template">Argentina</p></a></li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <ul>
-                                <li><p class="item-info-wine">Região</p></li>
-                                <li><a href="/regiao/mendoza"><p class="info-vinho-template">Mendoza</p></a></li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <ul>
-                                <li><p class="item-info-wine">Safra</p></li>
-                                <li><p class="">2013</p></li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <ul>
-                                <li><p class="item-info-wine">Tipo</p></li>
-                                <li><a href="/tipo-de-vinho/branco-seco"><p class="info-vinho-template">Branco Seco</p></a>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <ul>
-                                <li><p class="item-info-wine">Uva</p></li>
-                                <li><p class="">Chardonnay (100%)</p></li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <ul>
-                                <li><p class="item-info-wine">Volume</p></li>
-                                <li><p class="">750 ml</p></li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <ul>
-                                <li><p class="item-info-wine">Teor alcólico</p></li>
-                                <li><p class="">14%</p></li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <ul>
-                                <li><p class="item-info-wine">Temperatura de Serviço</p></li>
-                                <li><p class="">9 a 11ºC</p></li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <ul>
-                                <li><p class="item-info-wine">Corpo</p></li>
-                                <li><p class="">Encorpado</p></li>
-                            </ul>
-                        </li>
-
-                        <div class="rule-details-wine">
-
+                        @if($product->hasProducer())
                             <li>
                                 <ul>
-                                    <li><p class="item-info-wine">Sugestão de decantação</p></li>
-                                    <li><p class="">Não</p></li>
-                                </ul>
-                            </li>
-
-                            <li>
-                                <ul>
-                                    <li><p class="item-info-wine">Sugestão de guarda</p></li>
-                                    <li><p class="">de 5 até 10 anos</p></li>
-                                </ul>
-                            </li>
-
-                            <li>
-                                <ul>
-                                    <li><p class="item-info-wine">Combinações</p></li>
-                                    <li><p class="">Peixes, frutos do mar e bacalhau.</p></li>
-                                </ul>
-                            </li>
-
-                            <li>
-                                <ul>
-                                    <li><p class="item-info-wine">Validade</p></li>
-                                    <li><p class="">Válido por prazo indeterminado desde que conservado deitado em local
-                                            fresco e escuro.</p></li>
-                                </ul>
-                            </li>
-
-                            <li>
-                                <ul>
-                                    <li><p class="item-info-wine">Vinhedo</p></li>
-                                    <li><p class="">Adrianna, com altitude de 1480m. Colheita manual e rendimento
-                                            limitado.</p></li>
-                                </ul>
-                            </li>
-
-                            <li>
-                                <ul>
-                                    <li><p class="item-info-wine">Vinificação</p></li>
-                                    <li><p class="">Fermentação em barricas de carvalho francês, com leveduras selecionadas
-                                            e temperatura controlada por 20 dias.</p></li>
-                                </ul>
-                            </li>
-
-                            <li>
-                                <ul>
-                                    <li><p class="item-info-wine">Maturação</p></li>
-                                    <li><p class="">Maturado 11 meses em barricas de carvalho francês, sendo 73% novas.</p>
+                                    <li><p class="item-info-wine">Produtor</p></li>
+                                    <li><a href="{{ $product->producer->getWebUrl() }}"><p
+                                                    class="info-vinho-template">{{ $product->producer->name }}</p></a>
                                     </li>
                                 </ul>
                             </li>
+                        @endif
 
-                        </div>
+                        @if($product->hasCountry())
+                            <li>
+                                <ul>
+                                    <li><p class="item-info-wine">País</p></li>
+                                    <li><a href="{{ $product->country->getWebUrl() }}"><p
+                                                    class="info-vinho-template">{{ $product->country->name }}</p></a>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endif
+                        @if($product->hasRegion())
+                            <li>
+                                <ul>
+                                    <li><p class="item-info-wine">Região</p></li>
+                                    <li><a href="{{ $product->region->getWebUrl() }}"><p
+                                                    class="info-vinho-template">{{ $product->region->name }}</p></a>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endif
+                        @if($product->hasProductType())
+                            <li>
+                                <ul>
+                                    <li><p class="item-info-wine">Tipo</p></li>
+                                    <li><a href="{{ $product->productType->getWebUrl() }}"><p
+                                                    class="info-vinho-template">{{ $product->productType->name }}</p>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endif
 
-                        <a href="javascript:void(0);" class="see-more-info"> Veja mais</a>
 
+
+
+                            <?php $i = 1; $div = false; ?>
+                            @foreach($product->getAttributes() as $attribute)
+
+                                @if (! empty($attribute->getValue()))
+
+                                    @if ($i == 5)
+                                        <?php $div = true; ?>
+                                        <div class="rule-details-wine">
+                                            @endif
+
+                                            <li>
+                                                <ul>
+                                                    <li>
+                                                        <p class="item-info-wine">{{ $attribute->getAttribute()->getName() }}</p>
+                                                    </li>
+                                                    <li><p class="">{{ $attribute->getValue() }}</p></li>
+                                                </ul>
+                                            </li>
+                                            <?php $i++; ?>
+
+                                            @endif
+
+                                            @endforeach
+
+                                            @if($div == true)
+                                        </div>
+                                        <a href="javascript:void(0);" class="see-more-info"> Veja mais</a>
+                                    @endif
+
+
+                      
                     </ul>
 
 
@@ -385,11 +345,13 @@
                                 <span>Kaiken</span>
                             </a>
                         </h3>
-                        <p class="wine-intro">Aurelio Montes de Campo combinou as castas Malbec, Bonarda e Petit verdot para
+                        <p class="wine-intro">Aurelio Montes de Campo combinou as castas Malbec, Bonarda e Petit verdot
+                            para
                             ...</p>
                         <div class="content-card-product">
                             <div class="thumb-wine">
-                                <img class="label-wine" src="{{ asset_web('images/selo-pontos.png') }}" alt="Selo Vinho">
+                                <img class="label-wine" src="{{ asset_web('images/selo-pontos.png') }}"
+                                     alt="Selo Vinho">
                                 <a href="javascript:void(0);">
                                     <img class="wine-bottle" src="{{ asset_web('images/img-vinho.jpg') }}" alt="Vinho">
                                 </a>
@@ -423,11 +385,13 @@
                                 <span>Kaiken</span>
                             </a>
                         </h3>
-                        <p class="wine-intro">Aurelio Montes de Campo combinou as castas Malbec, Bonarda e Petit verdot para
+                        <p class="wine-intro">Aurelio Montes de Campo combinou as castas Malbec, Bonarda e Petit verdot
+                            para
                             ...</p>
                         <div class="content-card-product">
                             <div class="thumb-wine">
-                                <img class="label-wine" src="{{ asset_web('images/selo-pontos.png') }}" alt="Selo Vinho">
+                                <img class="label-wine" src="{{ asset_web('images/selo-pontos.png') }}"
+                                     alt="Selo Vinho">
                                 <a href="javascript:void(0);">
                                     <img class="wine-bottle" src="{{ asset_web('images/img-vinho.jpg') }}" alt="Vinho">
                                 </a>
@@ -461,11 +425,13 @@
                                 <span>Kaiken</span>
                             </a>
                         </h3>
-                        <p class="wine-intro">Aurelio Montes de Campo combinou as castas Malbec, Bonarda e Petit verdot para
+                        <p class="wine-intro">Aurelio Montes de Campo combinou as castas Malbec, Bonarda e Petit verdot
+                            para
                             ...</p>
                         <div class="content-card-product">
                             <div class="thumb-wine">
-                                <img class="label-wine" src="{{ asset_web('images/selo-pontos.png') }}" alt="Selo Vinho">
+                                <img class="label-wine" src="{{ asset_web('images/selo-pontos.png') }}"
+                                     alt="Selo Vinho">
                                 <a href="javascript:void(0);">
                                     <img class="wine-bottle" src="{{ asset_web('images/img-vinho.jpg') }}" alt="Vinho">
                                 </a>
@@ -499,11 +465,13 @@
                                 <span>Kaiken</span>
                             </a>
                         </h3>
-                        <p class="wine-intro">Aurelio Montes de Campo combinou as castas Malbec, Bonarda e Petit verdot para
+                        <p class="wine-intro">Aurelio Montes de Campo combinou as castas Malbec, Bonarda e Petit verdot
+                            para
                             ...</p>
                         <div class="content-card-product">
                             <div class="thumb-wine">
-                                <img class="label-wine" src="{{ asset_web('images/selo-pontos.png') }}" alt="Selo Vinho">
+                                <img class="label-wine" src="{{ asset_web('images/selo-pontos.png') }}"
+                                     alt="Selo Vinho">
                                 <a href="javascript:void(0);">
                                     <img class="wine-bottle" src="{{ asset_web('images/img-vinho.jpg') }}" alt="Vinho">
                                 </a>
