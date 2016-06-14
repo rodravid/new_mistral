e2391d94fada7dc60c19e852f6e641377b82ee81
André Teves <andreteves@gmail.com>
2016-06-14 12:42:59 -0300

    ajuste script deploy (2)

---
+++ deploy/after_restart.rb
@@ -0,0 +1,17 @@
+node[:deploy].each do |app_name, deploy|
+  current_path = "#{deploy[:deploy_to]}/current"
+  script "composer_install_and_chmod" do
+    interpreter "bash"
+    user "root"
+    cwd "#{current_path}"
+    code <<-EOH
+    rm -R storage/framework/cache
+    ln -s #{current_path}/storage/app/public/ #{current_path}/public/storage
+    chmod -R 777 bootstrap/cache
+    /usr/bin/php artisan cache:clear
+    /usr/bin/php artisan route:cache
+    /usr/bin/php artisan config:cache
+    chmod -R 777 storage
+    EOH
+  end
+end 
