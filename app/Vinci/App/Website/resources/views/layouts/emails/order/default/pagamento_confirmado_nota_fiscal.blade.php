260703dc7e14f62db39f834ca88627e617ee2a15
webeleven <WSilva@mac-wellington.local>
2016-06-30 11:42:14 -0300

    loading center, template 8 novas cores

--- app/Vinci/App/Website/resources/views/layouts/emails/order/default/pagamento_confirmado_nota_fiscal.blade.php
+++ app/Vinci/App/Website/resources/views/layouts/emails/order/default/pagamento_confirmado_nota_fiscal.blade.php
@@ -2,11 +2,11 @@
 
 @section('body')
 
-    <span style="font-size: 20px; color: #14a68f !important;"><b>Pagamento confirmado</b></span><br /><br />
+    <span style="font-size: 20px; color: #11b6f0 !important;"><b>Pagamento confirmado</b></span><br /><br />
 
     @include('website::layouts.emails.order.default.layouts.partials.salutation')
 
-    O status do seu pedido nº <span style="font-size: 13px; color: #14a68f !important;"><b>{{ $order->number }}</b></span> foi enviado com sucesso.
+    O status do seu pedido nº <span style="font-size: 13px; color: #11b6f0 !important;"><b>{{ $order->number }}</b></span> foi enviado com sucesso.
 
     @include('website::layouts.emails.order.default.layouts.partials.additional_message')
 
--- app/Vinci/App/Website/resources/views/layouts/emails/order/default/pedido_dificuldade_faturamento.blade.php
+++ app/Vinci/App/Website/resources/views/layouts/emails/order/default/pedido_dificuldade_faturamento.blade.php
@@ -4,11 +4,11 @@
 
     @include('website::layouts.emails.order.default.layouts.partials.salutation')
 
-    Informamos que referente ao seu pedido nº. <span style="font-size: 13px; color: #14a68f !important;"><b>{{ $order->number }}</b></span> estamos com dificuldades em processar o faturamento.<br /><br />
+    Informamos que referente ao seu pedido nº. <span style="font-size: 13px; color: #11b6f0 !important;"><b>{{ $order->number }}</b></span> estamos com dificuldades em processar o faturamento.<br /><br />
 
     Lembramos que os problemas mais comuns são divergências na data de validade ou no código de segurança. Para outros casos, consulte sua operadora, antes de nos contatar.<br /><br />
 
-    Para confirmar seus dados de pagamento, por favor, entre em contato conosco através deste e-mail <span style="font-size: 13px; color: #14a68f !important;">info@vinci.com.br</span> ou ligue para nosso atendimento no telefone (11) 3130-4646 (Atenção: atendimento de Internet somente de segunda a sexta-feira das 9h às 18h e sábados das 9h às 13h - exceto feriados).<br /><br />
+    Para confirmar seus dados de pagamento, por favor, entre em contato conosco através deste e-mail <span style="font-size: 13px; color: #11b6f0 !important;">info@vinci.com.br</span> ou ligue para nosso atendimento no telefone (11) 3130-4646 (Atenção: atendimento de Internet somente de segunda a sexta-feira das 9h às 18h e sábados das 9h às 13h - exceto feriados).<br /><br />
 
     Importante lembrar que o prazo de entrega do seu respectivo pedido somente é contado a partir da confirmação de pagamento.<br /><br />
 
