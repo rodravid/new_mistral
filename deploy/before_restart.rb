1cf65e9845d24ec7f4f9854e136e9879286a4e5d
André Teves <andreteves@gmail.com>
2016-07-03 10:45:18 -0300

    removendo supervisor do deploy hook

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -10,21 +10,4 @@ node[:deploy].each do |app_name, deploy|
     gulp --production 
     EOH
   end
-  
-  run_context.include_recipe "supervisor"
-  
-  # http://supervisord.org/configuration.html#program-x-section-values
-  # https://serversforhackers.com/monitoring-processes-with-supervisord
-  supervisor_service "laravel_worker" do
-    action :enable
-    process_name "%(program_name)s_%(process_num)02d"
-    command "php #{current_path} queue:work --queue=emails --daemon --tries=3"
-    autostart true
-    autorestart true
-    numprocs 2
-    redirect_stderr true
-    stdout_logfile "/tmp/worker.out.log"
-    stderr_logfile "/tmp/worker.err.log"
-  end
-  
 end 
