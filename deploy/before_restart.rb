f041f85219c1de80b2e61f801f463cca67a6910d
André Teves <andreteves@gmail.com>
2016-07-04 12:53:06 -0300

    testando filas sem o supervisor

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -6,6 +6,8 @@ node[:deploy].each do |app_name, deploy|
     cwd "#{current_path}"
     code <<-EOH
     ln -s #{current_path}/storage/app/public/ #{current_path}/public/storage
+    pkill -f artisan
+    nohup php artisan queue:work --queue=emails --daemon --tries=3 > /dev/null 2>1 &
     npm install
     gulp --production 
     EOH
