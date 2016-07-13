a7e95ad3ec0834a2291b19851d83a8f8547bfbc0
Felipe <falcantara@webeleven.com.br>
2016-07-13 15:20:30 -0300

    Ajustes

--- app/Vinci/App/Cms/Http/TestController.php
+++ app/Vinci/App/Cms/Http/TestController.php
@@ -3,42 +3,25 @@
 namespace Vinci\App\Cms\Http;
 
 use Doctrine\ORM\EntityManagerInterface;
-use Vinci\Domain\Customer\Customer;
-use Vinci\Domain\Product\Repositories\ProductRepository;
-use Vinci\Domain\ShoppingCart\ShoppingCart;
+use Illuminate\Contracts\Queue\Factory as QueueManager;
 
 class TestController extends Controller
 {
+    private $queue;
 
-    private $productRepository;
-
-    public function __construct(EntityManagerInterface $em, ProductRepository $productRepository)
+    public function __construct(EntityManagerInterface $em, QueueManager $queue)
     {
         parent::__construct($em);
 
-        $this->productRepository = $productRepository;
+        $this->queue = $queue;
     }
 
     public function index()
     {
-//        $product = $this->productRepository->find(1);
-//
-//        //$product->setCurrentChannel('teste');
-//
-//        dd('Produto ' . $product->getTitle() . ': R$ ' . number_format($product->getPrice()->asSalePrice(), 2, ',', '.'));
 
+        $job = $this->queue->connection('sqs')->pushOn('vinci-teste', new TestJob());
 
-
-       $cart = $this->entityManager->getRepository(ShoppingCart::class)->find('7a5485e3-c35a-49db-bdd1-9da4ce9c19c4');
-
-
-        $cart = new ShoppingCart();
-
-
-        dd($cart->getId() == '7a5485e3-c35a-49db-bdd1-9da4ce9c19c4');
-
-        $this->entityManager->persist($cart);
-        $this->entityManager->flush();
+        dd($job);
 
     }
 
