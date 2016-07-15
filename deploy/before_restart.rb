005cc70e9b5dfce225a0d55437946e487e5a2241
Thiago Cavallini <tcavallini@webeleven.com.br>
2016-07-15 14:50:52 -0300

    Update before_restart.rb

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -5,8 +5,6 @@ node[:deploy].each do |app_name, deploy|
     user "root"
     cwd "#{current_path}"
     code <<-EOH
-    ln -s #{current_path}/storage/app/public/ #{current_path}/public/storage
-    nohup php artisan queue:work --queue=vinci-queue --tries=3 --daemon > /dev/null 2>1 &
     npm install
     gulp --production 
     EOH
