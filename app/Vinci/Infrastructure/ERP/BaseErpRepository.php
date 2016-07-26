0cc16fe9e1d46133c39794fcea3654f13a30b518
Felipe <falcantara@webeleven.com.br>
2016-07-26 09:07:00 -0300

    Ajustes

--- app/Vinci/Infrastructure/ERP/BaseErpRepository.php
+++ app/Vinci/Infrastructure/ERP/BaseErpRepository.php
@@ -5,6 +5,7 @@ namespace Vinci\Infrastructure\ERP;
 use Exception;
 use Illuminate\Contracts\Config\Repository;
 use Spatie\Fractal\Fractal;
+use Vinci\App\Integration\Exceptions\IntegrationException;
 use Vinci\Infrastructure\Exceptions\EmptyResponseException;
 
 abstract class BaseErpRepository
