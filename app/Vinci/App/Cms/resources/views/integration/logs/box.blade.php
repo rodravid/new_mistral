d99dacb5a8badaa16bc13b98c74463e102603740
Felipe <falcantara@webeleven.com.br>
2016-07-28 13:55:13 -0300

    Ajustes

--- app/Vinci/App/Cms/resources/views/integration/logs/box.blade.php
+++ app/Vinci/App/Cms/resources/views/integration/logs/box.blade.php
@@ -7,16 +7,16 @@
             <table class="table table-bordered table-striped">
                 <thead>
                 <tr>
-                    <th>#ID</th>
-                    <th><i class="fa fa-user"></i> Usuário</th>
+                    <th style="width: 50px;">#ID</th>
+                    <th style="width: 15%;"><i class="fa fa-user"></i> Usuário</th>
+                    <th style="width: 10%;"><i class="fa fa-edit"></i> Status</th>
+                    <th><i class="fa fa-edit"></i> Mensagem</th>
                     @if(isset($with['request_type']))
                         <th><i class="fa fa-edit"></i> Tipo</th>
                     @endif
-                    <th><i class="fa fa-edit"></i> Status</th>
-                    <th><i class="fa fa-edit"></i> Mensagem</th>
-                    <th><i class="fa fa-calendar"></i> Data</th>
+                    <th style="width: 10%;"><i class="fa fa-calendar"></i> Data</th>
                     @if($loggedUser->isSuperAdmin())
-                        <th><i class="fa fa-edit"></i> Ações</th>
+                        <th style="width: 10%;"><i class="fa fa-edit"></i> Ações</th>
                     @endif
                 </tr>
                 </thead>
@@ -24,11 +24,11 @@
                     <tr>
                         <td>{{ $log->id }}</td>
                         <td>{{ $log->user }}</td>
+                        <td>{!! $log->present()->status_html !!}</td>
+                        <td>{{ $log->message }}</td>
                         @if(isset($with['request_type']))
                             <td>{!! $log->present()->request_type !!}</td>
                         @endif
-                        <td>{!! $log->present()->status_html !!}</td>
-                        <td>{{ $log->message }}</td>
                         <td>{{ $log->present()->created_at }}</td>
                         @if($loggedUser->isSuperAdmin())
                             <td><div class="btn-group btn-group-xs"><a href="{{ route('cms.integration.logs.show', [$log->resource_type, $log->id]) }}" target="_blank" class="btn btn-info"><i class="fa fa-eye"></i> Visualizar</a></div></td>
