50f497824534d719a646f3b347fb27bb37955767
Thiago Cavallini <tcavallini@webeleven.com.br>
2016-08-16 15:53:03 -0300

    Ajustes

--- app/Vinci/App/Website/resources/views/auth/passwords/reset.blade.php
+++ app/Vinci/App/Website/resources/views/auth/passwords/reset.blade.php
@@ -25,81 +25,84 @@
         <article class="wrap-content-register">
 
             <div class="pg-log">
-                   <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
-                        {!! csrf_field() !!}
+                <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
+                    {!! csrf_field() !!}
 
-                        <input type="hidden" name="token" value="{{ $token }}">
+                    <input type="hidden" name="token" value="{{ $token }}">
 
-                        <ul class="list-form-register">
+                    <ul class="list-form-register">
 
                         <li>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-Mail</label>
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label class="col-md-4 control-label">E-Mail</label>
 
-                            <div class="col-md-6">
-                                <input type="email" class="email input-register full" name="email" value="{{ $email or old('email') }}">
+                                <div class="col-md-6">
+                                    <input type="email" class="email input-register full" name="email"
+                                           value="{{ $email or old('email') }}">
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
+                                    @if ($errors->has('email'))
+                                        <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
                         </li>
 
                         <li>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Nova senha</label>
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <label class="col-md-4 control-label">Nova senha</label>
 
-                            <div class="col-md-6">
-                                <input type="password" class="email input-register full" name="password">
+                                <div class="col-md-6">
+                                    <input type="password" class="email input-register full" name="password">
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
+                                    @if ($errors->has('password'))
+                                        <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
                         </li>
 
                         <li>
 
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Confirmar Nova senha</label>
-                            <div class="col-md-6">
-                                <input type="password" class="email input-register full" name="password_confirmation">
+                            <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                                <label class="col-md-4 control-label">Confirmar nova senha</label>
+                                <div class="col-md-6">
+                                    <input type="password" class="email input-register full"
+                                           name="password_confirmation">
 
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="help-block">
+                                    @if ($errors->has('password_confirmation'))
+                                        <span class="help-block">
                                         <strong>{{ $errors->first('password_confirmation') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
                         </li>
 
                         <li>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                              <!--   <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-refresh"></i>Reset Password
-                                </button> -->
-                                <button type="submit" class="bt-default-full bt-middle template1">Salvar nova senha <span class="arrow-link">></span></button>
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <!--   <button type="submit" class="btn btn-primary">
+                                          <i class="fa fa-btn fa-refresh"></i>Reset Password
+                                      </button> -->
+                                    <button type="submit" class="bt-default-full bt-middle template1">Salvar nova senha
+                                        <span class="arrow-link">></span></button>
+                                </div>
                             </div>
-                        </div>
 
                         </li>
 
-                        </ul>
-                    </form>
+                    </ul>
+                </form>
             </div>
 
 
