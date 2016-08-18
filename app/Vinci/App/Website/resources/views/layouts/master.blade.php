2c68043854d95e529e83b85fea4f3eb9585725e4
Thiago Cavallini <tcavallini@webeleven.com.br>
2016-08-18 15:29:28 -0300

    retirada do analytics pois exibe pelo tag manager

--- app/Vinci/App/Website/resources/views/layouts/master.blade.php
+++ app/Vinci/App/Website/resources/views/layouts/master.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <title>@yield('title', 'Vinci, somos loucos por vinho - Vinci')</title>
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset_web('images/vinci_ico.ico') }}">
-    <meta name="description" content="@yield('description', 'A Vinci é uma das mais premiadas importadoras de vinhos do país, com uma seleção de vinhos e produtores mundialmente aclamados em um catálogo imperdível. Veja!')">
+    <meta name="description"
+          content="@yield('description', 'A Vinci é uma das mais premiadas importadoras de vinhos do país, com uma seleção de vinhos e produtores mundialmente aclamados em um catálogo imperdível. Veja!')">
     @section('styles')
         <link rel="stylesheet" href="{{ asset_web('css/style.min.css') }}">
         <link rel="stylesheet" href="{{ asset_web('js/sweetalert/sweetalert.css') }}">
@@ -22,25 +23,23 @@
 </head>
 <body>
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-5572168-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
-
 <!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WWHF7K"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WWHF7K');</script>
+<noscript>
+    <iframe src="//www.googletagmanager.com/ns.html?id=GTM-WWHF7K"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+<script>(function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            'gtm.start': new Date().getTime(), event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+                '//www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-WWHF7K');</script>
 <!-- End Google Tag Manager -->
 
 @yield('content')
@@ -62,7 +61,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <script src="{{ asset_web('js/input-mask/jquery.inputmask.js') }}" type="text/javascript"></script>
         <script src="{{ asset_web('js/input-mask/jquery.inputmask.extensions.js') }}" type="text/javascript"></script>
         <script src="{{ asset_web('js/jquery.placeholder.js') }}" type="text/javascript"></script>
-        <script src="{{ asset_web('js/jquery-typeahead/dist/jquery.typeahead.min.js') }}" type="text/javascript"></script>
+        <script src="{{ asset_web('js/jquery-typeahead/dist/jquery.typeahead.min.js') }}"
+                type="text/javascript"></script>
         <script src="{{ asset('assets/common/js/address-autocomplete.js') }}" type="text/javascript"></script>
         <script src="{{ asset_web('js/script.js') }}" type="text/javascript"></script>
 
@@ -73,26 +73,41 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <script src="{{ asset_web('app/js/services/cart.js') }}" type="text/javascript"></script>
         <script src="{{ asset_web('app/js/services/favorite.js') }}" type="text/javascript"></script>
         <script src="{{ asset_web('app/js/controllers/home/homeController.js') }}" type="text/javascript"></script>
-        <script src="{{ asset_web('app/js/controllers/auth/modalLoginController.js') }}" type="text/javascript"></script>
-        <script src="{{ asset_web('app/js/controllers/auth/modalPasswordController.js') }}" type="text/javascript"></script>
-        <script src="{{ asset_web('app/js/controllers/product/productPageController.js') }}" type="text/javascript"></script>
-        <script src="{{ asset_web('app/js/controllers/register/registerController.js') }}" type="text/javascript"></script>
-        <script src="{{ asset_web('app/js/controllers/address/addressModalController.js') }}" type="text/javascript"></script>
+        <script src="{{ asset_web('app/js/controllers/auth/modalLoginController.js') }}"
+                type="text/javascript"></script>
+        <script src="{{ asset_web('app/js/controllers/auth/modalPasswordController.js') }}"
+                type="text/javascript"></script>
+        <script src="{{ asset_web('app/js/controllers/product/productPageController.js') }}"
+                type="text/javascript"></script>
+        <script src="{{ asset_web('app/js/controllers/register/registerController.js') }}"
+                type="text/javascript"></script>
+        <script src="{{ asset_web('app/js/controllers/address/addressModalController.js') }}"
+                type="text/javascript"></script>
         <script src="{{ asset_web('app/js/directives/cart/cartItem.js') }}" type="text/javascript"></script>
         <script src="{{ asset_web('app/js/directives/cart/cartAddButton.js') }}" type="text/javascript"></script>
         <script src="{{ asset_web('app/js/directives/product/favoriteWidget.js') }}" type="text/javascript"></script>
         <script src="{{ asset_web('app/js/controllers/cart/cartController.js') }}" type="text/javascript"></script>
-        <script src="{{ asset_web('app/js/controllers/cart/cartWidgetController.js') }}" type="text/javascript"></script>
-        <script src="{{ asset_web('app/js/controllers/search/search-filters-controller.js') }}" type="text/javascript"></script>
-        <script src="{{ asset_web('app/js/directives/search/search-filters-directive.js') }}" type="text/javascript"></script>
-        <script src="{{ asset_web('app/js/directives/search/search-suggestion-directive.js') }}" type="text/javascript"></script>
-        <script src="{{ asset_web('app/js/showcase/directives/showcase-container-directive.js') }}" type="text/javascript"></script>
-        <script src="{{ asset_web('app/js/showcase/directives/showcase-widget-directive.js') }}" type="text/javascript"></script>
-        <script src="{{ asset_web('app/js/newsletter/directives/newsletter-widget-directive.js') }}" type="text/javascript"></script>
-        <script src="{{ asset_web('app/js/newsletter/services/newsletter-service.js') }}" type="text/javascript"></script>
+        <script src="{{ asset_web('app/js/controllers/cart/cartWidgetController.js') }}"
+                type="text/javascript"></script>
+        <script src="{{ asset_web('app/js/controllers/search/search-filters-controller.js') }}"
+                type="text/javascript"></script>
+        <script src="{{ asset_web('app/js/directives/search/search-filters-directive.js') }}"
+                type="text/javascript"></script>
+        <script src="{{ asset_web('app/js/directives/search/search-suggestion-directive.js') }}"
+                type="text/javascript"></script>
+        <script src="{{ asset_web('app/js/showcase/directives/showcase-container-directive.js') }}"
+                type="text/javascript"></script>
+        <script src="{{ asset_web('app/js/showcase/directives/showcase-widget-directive.js') }}"
+                type="text/javascript"></script>
+        <script src="{{ asset_web('app/js/newsletter/directives/newsletter-widget-directive.js') }}"
+                type="text/javascript"></script>
+        <script src="{{ asset_web('app/js/newsletter/services/newsletter-service.js') }}"
+                type="text/javascript"></script>
 
-        <script src="{{ asset_web('app/js/shipping/directives/shippingDeadline-widget-directive.js') }}" type="text/javascript"></script>
-        <script src="{{ asset_web('app/js/shipping/services/shippingDeadline-service.js') }}" type="text/javascript"></script>
+        <script src="{{ asset_web('app/js/shipping/directives/shippingDeadline-widget-directive.js') }}"
+                type="text/javascript"></script>
+        <script src="{{ asset_web('app/js/shipping/services/shippingDeadline-service.js') }}"
+                type="text/javascript"></script>
 
         <script src="{{ asset_web('js/angular/angular-input-masks/releases/angular-input-masks-standalone.min.js') }}"></script>
 
@@ -103,7 +118,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     @if (Session::has('flash_notification.message'))
         <script>
 
-            (function() {
+            (function () {
 
                 var level = '{{ Session::get('flash_notification.level') }}';
                 var message = '{{ Session::get('flash_notification.message') }}';
