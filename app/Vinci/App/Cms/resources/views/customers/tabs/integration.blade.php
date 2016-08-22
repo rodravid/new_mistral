cd2f9fa3e9cc6c4573c7bdb1d14df5eee41da7b5
Felipe <falcantara@webeleven.com.br>
2016-08-22 16:53:16 -0300

    Ajustes cms

--- app/Vinci/App/Cms/resources/views/customers/tabs/integration.blade.php
+++ app/Vinci/App/Cms/resources/views/customers/tabs/integration.blade.php
@@ -6,11 +6,15 @@
                 <a href="javascript:void(0);" class="btn btn-success"
                    data-form-link
                    data-method="POST"
+                   data-confirm-title="Confirmação de exportação"
+                   data-confirm-text="Deseja realmente exportar o cliente agora?"
                    data-params='{"current-tab":"#customer-integration"}'
                    data-action="{{ route('cms.customers.edit#export-erp', [$customer->getId()]) }}"><i class="fa fa-cloud-upload"></i> Exportar agora</a>
                 <a href="javascript:void(0);" class="btn btn-primary"
                    data-form-link
                    data-method="POST"
+                   data-confirm-title="Confirmação de envio"
+                   data-confirm-text="Deseja realmente enviar o cliente para a fila de integração?"
                    data-params='{"current-tab":"#customer-integration"}'
                    data-action="{{ route('cms.customers.edit#export-erp-queue', [$customer->getId()]) }}"><i class="glyphicon glyphicon-export"></i> Enviar para fila de integração</a>
             </div>
