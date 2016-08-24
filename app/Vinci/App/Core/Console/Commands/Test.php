a10b1c67f092c06faf094af4c21f4ca3094caf6f
Felipe <falcantara@webeleven.com.br>
2016-08-24 09:26:00 -0300

    Ajustes

--- app/Vinci/App/Core/Console/Commands/Test.php
+++ app/Vinci/App/Core/Console/Commands/Test.php
@@ -28,7 +28,31 @@ class Test extends Command
      */
     public function handle()
     {
-        dd(QueueWorker::listActiveQueueWorkers());
+
+        //dd(QueueWorker::listActiveQueueWorkers());
+
+        $urlPattern = 'http://www1.vinci.com.br/assets/website/images/wine_seals/decanter%s';
+
+        exec('ls -la public/assets/website/images/wine_seals/decanter | grep .png', $output);
+
+        $lines = collect($output);
+
+        $lines->transform(function($line) use ($urlPattern) {
+
+            $segments = explode(' ', $line);
+
+            $imgName = end($segments);
+
+            return sprintf($urlPattern, $imgName);
+
+        });
+
+        foreach ($lines as $line) {
+
+            file_put_contents(storage_path('app/seals.txt'), $line . PHP_EOL, FILE_APPEND);
+
+        }
+
     }
 
 }
