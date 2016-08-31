b54961e93aa7d297f71599f2483ab8667dada7f8
Felipe <falcantara@webeleven.com.br>
2016-08-31 13:21:35 -0300

    Ajustes

--- tests/ProductSerializationTest.php
+++ tests/ProductSerializationTest.php
@@ -6,18 +6,7 @@ class ProductSerializationTest extends TestCase
     public function testSearializeAnProduct()
     {
 
-        $product = app(\Vinci\Domain\Country\CountryRepository::class)->find(2);
-        
 
-        $serialized = serialize($product);
-
-        $teste = unserialize($serialized);
-
-        $teste->setName('África do Sullll');
-
-        app('em')->persist($teste);
-
-        app('em')->flush();
 
     }
 }
