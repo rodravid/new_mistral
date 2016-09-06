1265826db83aacbf0a5df4215fea6fed9cf1a39c
webeleven <WSilva@mac-wellington.local>
2016-09-06 11:24:06 -0300

    menu alterado

--- app/Vinci/App/Website/resources/views/layouts/menu.blade.php
+++ app/Vinci/App/Website/resources/views/layouts/menu.blade.php
@@ -364,7 +364,7 @@
 
             <li class="menu-item larger760-right template12">
                 <a class="menu-link" href="/c/meias-garrafas">
-                    Meias garrafas
+                   Mini e Meias garrafas
                 </a>
                 <div class="drop-menu">
                     <div class="division">
@@ -383,7 +383,7 @@
                         <a href="/c/meias-garrafas">
                             <div class="every-category">
                                 <h3>Veja todos os vinhos
-                                    em meia garrafa</h3>
+                                    em mini e meias garrafas</h3>
                                 <span>></span>
                             </div>
                         </a>
