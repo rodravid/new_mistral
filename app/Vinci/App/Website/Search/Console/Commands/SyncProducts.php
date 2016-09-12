a33e9930f800eccf6e9a5f1889496c8e36d741a2
Felipe <falcantara@webeleven.com.br>
2016-09-12 09:19:45 -0300

    Ajustes ordenacao

--- app/Vinci/App/Website/Search/Console/Commands/SyncProducts.php
+++ app/Vinci/App/Website/Search/Console/Commands/SyncProducts.php
@@ -2,6 +2,7 @@
 
 namespace Vinci\App\Website\Search\Console\Commands;
 
+use Exception;
 use Illuminate\Console\Command;
 use Vinci\Domain\Search\Product\ProductIndexerService;
 
@@ -13,7 +14,7 @@ class SyncProducts extends Command
      *
      * @var string
      */
-    protected $signature = 'search:sync { --index }';
+    protected $signature = 'search:sync { product? } { --index }';
 
     /**
      * The console command description.
@@ -39,6 +40,26 @@ class SyncProducts extends Command
      */
     public function handle()
     {
+
+        if ($productId = $this->argument('product')) {
+
+            $this->message(sprintf("Indexing product %s in elasticsearch...", $productId));
+
+            try {
+
+                $this->indexerService->syncOne($productId);
+
+                $this->info("\nDone!");
+
+            } catch (Exception $e) {
+
+                $this->error(sprintf("Error: %s", $e->getMessage()));
+
+            }
+
+            return true;
+        }
+
         if ($this->option('index')) {
             $this->runIndexCreation();
         }
