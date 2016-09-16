f61a3f5fd5f35cd02f5313ae43a0159ff7e68886
rodravid <roliveira@webeleven.com.br>
2016-09-16 00:53:03 -0300

    [CMS - Orders -> List] Ajuste em desempenho

--- app/Vinci/App/Cms/resources/views/orders/list.blade.php
+++ app/Vinci/App/Cms/resources/views/orders/list.blade.php
@@ -117,28 +117,5 @@
 
 @section('scripts')
     @parent
-
-    <script type="text/javascript">
-
-        $(document).ready(function () {
-            $(".btn.btn-success").click(function (event) {
-                var itemsPerPage = $("#itemsPerPage").val();
-                $("#itemsPerPage").val('0');
-
-                var filters = $('#filters').serialize() + '&itemsPerPage=0';
-                window.open($(this).prop('href') + '?' + filters);
-
-                $("#itemsPerPage").val(itemsPerPage);
-
-                event.preventDefault();
-            });
-        });
-
-        $(function(){
-            $('#startDatePicker').datetimepicker();
-            $('#endDatePicker').datetimepicker();
-        });
-
-    </script>
-
+    <script src="{{ asset_cms('pages/orders/ordersListPage.js') }}"></script>
 @endsection
