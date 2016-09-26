8f8dfd1da14d7ac949f2f618d8c8b9fa382bdbb0
Felipe <falcantara@webeleven.com.br>
2016-09-26 15:37:51 -0300

    Ajustes

--- app/Vinci/App/Core/Http/Middleware/Authenticate.php
+++ app/Vinci/App/Core/Http/Middleware/Authenticate.php
@@ -24,7 +24,7 @@ class Authenticate
             if ($request->ajax() || $request->wantsJson()) {
                 return response('Unauthorized.', 401);
             } else {
-                return redirect()->intended($this->getLoginRedirect($guard));
+                return redirect()->guest($this->getLoginRedirect($guard));
             }
         }
 
